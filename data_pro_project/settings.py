7cb77ffdcf4b1e7e0ee2878a3944d9996be917d7
Akhil Biju Varghese <akhil.sbj@gmail.com>
2026-05-26 22:50:32 +0530

    Security fix
    Fix the security settings in data_pro_project/settings.py

--- data_pro_project/settings.py
+++ data_pro_project/settings.py
@@ -24,15 +24,12 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-replace-me-in-production')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost"
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
